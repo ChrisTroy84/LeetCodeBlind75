a59799b19d5313010053ce8c05cf682358462c31
Chris Troy <chris.troy@tamu.edu>
2023-10-12 20:41:53 -0700

    Add files via upload

---
+++ Array&Strings/RevereVowels.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    //helper funct to check if char is vowel
+    bool isVowel(char c) {
+        c = tolower(c);
+
+        return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+
+    }
+
+    string reverseVowels(string s) {
+
+        //two pouinters
+        int left = 0;
+        int right = s.size() - 1;
+
+        //while there are chars to traverse
+        while (left < right) {
+            //find leftmost vowel
+            while (left < s.size() && !isVowel(s[left])) {
+                left++;
+            }
+            //find rightmost vowel
+            while (right >= 0 && !isVowel(s[right])) {
+                right--;
+            }
+            
